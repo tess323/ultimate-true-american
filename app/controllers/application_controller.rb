a89c0550ae6c1929878a10e1139eb110d83a15de
tess323 <tess323@gmail.com>
2015-11-20 15:40:42 -0800

    s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
       redirect_to login_path
     end
   end
+  
 
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
